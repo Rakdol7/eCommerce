f3308c87481f16029d5b33d9604d22d9b058a02b
Rakdol7 <g.masnada07@gmail.com>
2024-11-22 08:32:18 +0100

    Add files via upload

---
+++ eCommerce/eCommerce/Carrello.cs
@@ -0,0 +1,48 @@
+﻿namespace eCommerce
+{
+    class Carrello
+    {
+        private string identificativo;
+
+        private List<Prodotto> prodotti;
+
+        public List<Prodotto> Prodotti
+        {
+            get { return prodotti; }
+        }
+        public string Identificativo
+        {
+            get { return identificativo; }
+        }
+        public void aggiungiProdotto(Prodotto prodotto)
+        {
+            if (prodotto == null)
+                return;
+            prodotti.Add(prodotto);
+        }
+        public void rimuoviProdotto(Prodotto prodotto)
+        {
+            if (prodotto == null)
+                return;
+            prodotti.Remove(prodotto);
+        }
+        public void rimuoviProdottoVistaInClasse(Prodotto prodotto)
+        {
+            if (prodotto == null)
+                return;
+            int pos = prodotti.IndexOf(prodotto);
+            if (pos != -1)
+                prodotti.RemoveAt(pos);
+        }
+        public void svuotaCarrello()
+        {
+            prodotti.Clear();
+        }
+
+        public Carrello(string identificativo)
+        {
+            this.identificativo = identificativo;
+            prodotti = new List<Prodotto>();
+        }
+    }
+}
